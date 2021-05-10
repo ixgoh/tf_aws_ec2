851bc77bf58e2d0ce030f86a61d52fce18d875b0
Goh Ie Xiann <ixgoh@outlook.com>
2021-05-11 00:05:45 +0800

    Add user script

--- main.tf
+++ main.tf
@@ -131,6 +131,14 @@ resource "aws_instance" "myapp-server" {
   associate_public_ip_address = true
   key_name = aws_key_pair.ssh-key.key_name
 
+  user_data = <<EOF
+                  #!/bin/bash
+                  sudo yum update -y && sudo yum install -y docker
+                  sudo systemctl start docker
+                  sudo usermod -aG docker $USER
+                  docker run -p 8080:80 nginx
+              EOF
+
   tags = {
     "Name" = "${var.environment}-server"
   }
@@ -139,3 +147,4 @@ resource "aws_instance" "myapp-server" {
 output "ec2_public_ip" {
   value = aws_instance.myapp-server.public_ip
 }
+
